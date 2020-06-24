f2331ae1f87cf593849789b226a9aba1fa74dcf4
LeandroDante LENOVO-DESKTOP <leandrodanteoliveira@hotmail.com>
2020-06-24 15:49:54 -0300

    Login Final

--- api/getLogin.php
+++ api/getLogin.php
@@ -4,15 +4,42 @@
     //$body = json_decode(file_get_contents('php://input'),true);
     $user = $_POST['user'];
     $pass = $_POST['pass'];
+    $comp = $_POST['comp'];
     $collection = $conn->lab2dev->access;
     $query = array(
-        'username' => $user,
+        'email' => $user,
         'password' => $pass
     );
     $result = $collection->find($query)->toArray();
-    $myJSON = json_encode($result);
-    
-    echo $myJSON;
+    $aggUni = array(
+        array(
+            '$lookup' => array(
+                "from" => 'company',
+                "localField" => 'fk_company',
+                "foreignField" => 'id',
+                "as" => 'comp_uni'
+            )
+        ),
+        array(
+            '$match' => array(
+                "comp_uni.code" => $comp
+            )
+        ),
+        array(
+            '$group' => array(
+                "_id" => '$_id',
+                "id_unidade" =>array('$first' => '$id'),
+            )
+        ),
+    );
+    $unidade = $conn->lab2dev->unity->aggregate($aggUni)->toArray();
+    if(( isset($unidade[0]) && $unidade[0]->id_unidade) == $result[0]->fk_uni ){
+        $jReturn = json_encode($result);
+        echo $jReturn;
+    }else{
+        echo '{"STATUS":"INVALID COMPANY"}';
+    }
     header('Content-Type: application/json');
     header('HTTP/1.0 200');
+    
 ?>
--- autentication.php
+++ autentication.php
@@ -3,16 +3,15 @@ header ("Access-Control-Allow-Origin: *");
 header ("Access-Control-Expose-Headers: Content-Length, X-JSON");
 header ("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
 header ("Access-Control-Allow-Headers: *");
-//if (!isset($_SERVER['PHP_AUTH_USER'])) {
-//    header('WWW-Authenticate: Basic realm="LAB2TEMP"');
-//    header('HTTP/1.0 401 Unauthorized');
-//    exit;
-//} else {
-//    if(!($_SERVER['PHP_AUTH_USER'] == 'dev' && $_SERVER['PHP_AUTH_PW'] == 'lab2dev'))
-//    {
-//        header('WWW-Authenticate: Basic realm="LAB2TEMP"');
-//        header('HTTP/1.0 401 Unauthorized');
-//        exit;
-//    }
-//}
+
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
+    //header('HTTP/1.1 401 Unauthorized');
+    die('INVALID_AUTENTICATION');
+} else {
+    if(!($_SERVER['PHP_AUTH_USER'] == 'dev' && $_SERVER['PHP_AUTH_PW'] == 'lab2dev'))
+    {
+        //header('HTTP/1.1 401 Unauthorized');
+        die('INVALID_AUTENTICATION');
+    }
+}
 ?>
