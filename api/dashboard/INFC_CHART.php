6326349539bd6f82f1a12be957e2026bfa9d2b28
LeandroDante LENOVO-DESKTOP <leandrodanteoliveira@hotmail.com>
2020-06-29 15:41:39 -0300

    INFC e SUSP Update

--- api/dashboard/INFC_CHART.php
+++ api/dashboard/INFC_CHART.php
@@ -1,9 +1,12 @@
 <?php
 
 require '../../autentication.php';
-header('Content-type: application/json');
-$return = [
-    "label" => [
+require '../../connection.php';
+$comp = $_POST['comp'];
+$type = $_POST['type'];
+$label = [];
+if ($type == 'm') {
+    $label = [
         "Janeiro",
         "Fevereiro",
         "Março",
@@ -16,20 +19,54 @@ $return = [
         "Outubro",
         "Novembro",
         "Dezembro"
-    ],
-    "data" => [
-        10,
-        12,
-        15,
-        13,
-        15,
-        14,
-        12,
-        11,
-        10,
-        17,
-        16,
-        18
-    ]
+    ];
+    $data = [];
+    for ($i=1; $i < 13; $i++) { 
+        $aggJUN = [
+            [
+                '$lookup' => [
+                    "from" => 'totem',
+                    "localField" => 'fk_totem',
+                    "foreignField" => 'id',
+                    "as" => 'totem'
+                ]
+            ],
+            [
+                '$project' => [
+                    'name' => 1,
+                    'month' => [
+                        '$month' => '$date_created'
+                    ],
+                    'comp' => '$totem.fk_company',
+                    'temp' => '$temp'
+                ]
+            ],
+            [
+                '$match' => [
+                    "comp" => intval($comp),
+                    'month' => $i,
+                    "temp" => [
+                        '$gte' => 37
+                    ]
+                ]
+            ],
+            [
+                '$count' => 'qtde'
+            ],
+        ];
+        $rMounth = $conn->lab2dev->totem_log->aggregate($aggJUN)->toArray();
+        if(!isset($rMounth[0]['qtde'])){
+            $rMounth[0]['qtde'] = 0;
+        }
+        array_push($data,$rMounth[0]['qtde']);
+    }
+    
+    
+}
+header('Content-type: application/json');
+
+$return = [
+    "label" => $label,
+    "data" => $data
 ];
 echo json_encode($return);
